ff3bfa3dd4db126c74ad2914239dd2e5a61762e6
Max Reis <maxzdosreis@gmail.com>
2026-05-30 00:54:59 -0300

    CHORE: cria projeto api e web, juntamente com o gitignore

---
+++ api/src/test/java/br/com/megaloja/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.megaloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
