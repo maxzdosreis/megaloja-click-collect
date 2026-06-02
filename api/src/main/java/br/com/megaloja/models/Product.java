3e3ff0536ba22f4da5b7a08f46044cc560eea0f8
Max Reis <maxzdosreis@gmail.com>
2026-06-02 19:40:49 -0300

    feat(produtos): adiciona entidade de Product

---
+++ api/src/main/java/br/com/megaloja/models/Product.java
@@ -0,0 +1,41 @@
+package br.com.megaloja.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "image_url", length = 500)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+}
