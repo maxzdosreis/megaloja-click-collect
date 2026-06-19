30668c91a4b43e8a1be6c16e5a819d145d4ed49e
Max Reis <maxzdosreis@gmail.com>
2026-06-19 18:40:20 -0300

    feat(docs): adiciona novas tags para os novos endpoints de Auth e User

--- api/src/main/java/br/com/megaloja/config/OpenAPIConfig.java
+++ api/src/main/java/br/com/megaloja/config/OpenAPIConfig.java
@@ -1,9 +1,12 @@
 package br.com.megaloja.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +31,8 @@ public class OpenAPIConfig {
                                 .name("MIT License")
                                 .url("https://opensource.org/licenses/MIT")))
                 .tags(List.of(
+                        new Tag().name("Autenticação").description("Endpoints de autenticação e geração de tokens JWT"),
+                        new Tag().name("Usuários").description("Gerenciamento de usuários do sistema"),
                         new Tag().name("Produtos").description("Gerenciamento de produtos do catálogo"),
                         new Tag().name("Lojas").description("Gerenciamento de lojas físicas"),
                         new Tag().name("Inventário").description("Consulta e atualização do estoque por loja e produto"),
@@ -35,6 +40,14 @@ public class OpenAPIConfig {
                         new Tag().name("Notificações").description("Notificações relacionadas aos pedidos"),
                         new Tag().name("Autorizações de Retirada")
                                 .description("Autorizações para terceiros retirarem pedidos")
-                ));
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .name("bearer-jwt")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Insira o token JWT obtido no endpoint /auth/login")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
 }
