abf618ceb6e2339aa4d6bf44614271c452b89e58
Max Reis <maxzdosreis@gmail.com>
2026-06-20 16:26:25 -0300

    fix: previne race condition na reserva de estoque

--- api/src/main/java/br/com/megaloja/repositories/InventoryRepository.java
+++ api/src/main/java/br/com/megaloja/repositories/InventoryRepository.java
@@ -1,8 +1,11 @@
 package br.com.megaloja.repositories;
 
 import br.com.megaloja.models.Inventory;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +14,8 @@ import java.util.Optional;
 public interface InventoryRepository extends JpaRepository<Inventory, Long>, JpaSpecificationExecutor<Inventory> {
 
     Optional<Inventory> findByStoreIdAndProductId(Long storeId, Long productId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT i FROM Inventory i WHERE i.store.id = :storeId AND i.product.id = :productId")
+    Optional<Inventory> findByStoreIdAndProductIdWithLock(Long storeId, Long productId);
 }
